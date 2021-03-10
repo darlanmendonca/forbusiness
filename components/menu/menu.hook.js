15216178c9dfae89575200c38e8ce0d25c5ca8f7
Darlan Mendonca <darlan.mendonca@vagas.com.br>
2021-03-10 15:03:15 -0300

    adicionando evento para sair do menu (tecla ESC)

--- components/menu/menu.hook.js
+++ components/menu/menu.hook.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export const useMenu = () => {
   const [ isVisible, setIsVisible ] = useState(false)
@@ -14,6 +14,20 @@ export const useMenu = () => {
       if (!hasParentMenu) hide()
     }, 0)
   }
+
+  const listenEscapeKey = () => {
+    const escape = event => event.key === 'Escape' && hide()
+
+    if (isVisible) {
+      document.addEventListener('keydown', escape)
+    } else {
+      document.removeEventListener('keydown', escape)
+    }
+
+    return () => document.removeEventListener('keydown', escape)
+  }
+
+  useEffect(listenEscapeKey, [ isVisible ])
 
   const buttonProps = {
     role: 'menuitem',
