e056acc23f9fba03c291ca1833704d856ecc9b3b
Darlan Mendonca <darlan.mendonca@vagas.com.br>
2021-03-22 19:07:11 -0300

    corrigindo mock de vagas modelo

--- pages/api/vagas-modelo.js
+++ pages/api/vagas-modelo.js
@@ -18,72 +18,22 @@ const juliana = {
 
 const templates = [
   {
-    id: 'v2167406',
+    id: 'm2167406',
     name: 'Auxiliar administrativo',
-    status: 'Agendada',
-    date: {
-      open: new Date('2021-03-01T00:00:00'),
-      expiration: new Date('2021-03-30T00:00:00'),
-    },
     location: 'Salvador',
     owner: baby,
   },
   {
-    id: 'v2167408',
-    name: 'Recepcionista',
-    status: 'Encerrada',
-    date: {
-      open: new Date('2021-03-01T00:00:00'),
-      expiration: new Date('2021-03-30T00:00:00'),
-    },
-    location: 'Salvador',
-    owner: juliana,
-  },
-  {
-    id: 'v2167409',
-    name: 'Analista de Testes',
-    level: 'Júnior',
-    status: 'Em andamento',
-    date: {
-      open: new Date('2021-03-01T00:00:00'),
-      expiration: new Date('2021-03-30T00:00:00'),
-    },
+    id: 'm2167311',
+    name: 'Engenheiro(a) Frontend',
+    level: 'Sênior',
     location: 'São Paulo',
     owner: moises,
   },
   {
-    id: 'v2167410',
-    name: 'Analista de Testes',
-    level: 'Pleno',
-    status: 'Em andamento',
-    date: {
-      open: new Date('2021-03-01T00:00:00'),
-      expiration: new Date('2021-03-30T00:00:00'),
-    },
-    location: 'Salvador, Home Office',
-    owner: moises,
-  },
-  {
-    id: 'v2167411',
-    name: 'Analista de Testes',
-    level: 'Sênior',
-    status: 'Em andamento',
-    date: {
-      open: new Date('2021-03-01T00:00:00'),
-      expiration: new Date('2021-03-30T00:00:00'),
-    },
-    location: 'Home Office',
-    owner: moises,
-  },
-  {
-    id: 'v2167411',
+    id: 'm2167312',
     name: 'Engenheiro(a) Frontend',
     level: 'Sênior',
-    status: 'Em andamento',
-    date: {
-      open: new Date('2021-03-01T00:00:00'),
-      expiration: new Date('2021-03-30T00:00:00'),
-    },
     location: 'Home Office',
     owner: moises,
   },
